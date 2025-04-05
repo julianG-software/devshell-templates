3004f1fc3409b7d18cfa2807d0c82318b745a4ab
Julian Gerke <julian.gerke99@gmail.com>
2025-04-05 17:11:25 +0200

    Initial commit
    Create Nix Termplate Structure
    Based on : https://github.com/NixOS/templates

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A collection of flake templates";
+
+  outputs = { self }: {
+
+    templates = {
+
+      default = {
+        path = ./default;
+        description = "A very basic flake";
+      };
+
+      typescript = {
+        path = ./typescript/;
+        description = "A template installing node,typescript and common dependencies";
+        welcomeText = ''
+          # Getting Started
+          - run `nix develop` to enter the development environment
+        '';
+      };
+    };
+
+    defaultTemplate = self.templates.default;
+
+  };
+}
